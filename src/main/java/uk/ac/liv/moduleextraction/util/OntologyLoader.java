04cc1da20d2a51fdd06e32dd8887bfb3aa37928d
Boris Konev <17253548+bkonev@users.noreply.github.com>
2017-07-28 09:23:13 +0100

    handling zipped / bzipped inputs

--- src/main/java/uk/ac/liv/moduleextraction/util/OntologyLoader.java
+++ src/main/java/uk/ac/liv/moduleextraction/util/OntologyLoader.java
@@ -1,5 +1,9 @@
 package uk.ac.liv.moduleextraction.util;
 
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+import org.apache.commons.compress.compressors.FileNameUtil;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
+import org.apache.commons.io.FilenameUtils;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.dlsyntax.renderer.DLSyntaxObjectRenderer;
 import org.semanticweb.owlapi.io.FileDocumentSource;
@@ -8,10 +12,12 @@ import org.semanticweb.owlapi.io.UnparsableOntologyException;
 import org.semanticweb.owlapi.model.*;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class OntologyLoader {
 
-		
 	public static OWLOntology loadOntologyAllAxioms(String pathName){
 
 		ToStringRenderer stringRenderer= new ToStringRenderer();
@@ -25,24 +31,42 @@ public class OntologyLoader {
 
 		OWLOntology ontology = null;
 		
-		FileDocumentSource source = new FileDocumentSource(new File(pathName));
-		MissingImportListener listener = importer -> System.out.println("Missing import");
-		manager.addMissingImportListener(listener);
-		
+		//FileDocumentSource source = new FileDocumentSource(new File(pathName));
 		try {
-			ontology =  
-					manager.loadOntologyFromOntologyDocument(source, configuration);
-			
+            // InputStream source = new FileInputStream(pathName);
+		    InputStream source = null;
+		    String extension = FilenameUtils.getExtension(pathName);
+		    switch (extension) {
+                case "bz2":
+                    source = new BZip2CompressorInputStream(new FileInputStream(pathName));
+                    break;
+                case "zip":
+                    source = new ZipArchiveInputStream(new FileInputStream(pathName));
+                    break;
+                default:
+                    // assume plain text
+                    source = new FileInputStream(pathName);
+            }
+            MissingImportListener listener = importer -> System.out.println("Missing import");
+			manager.addMissingImportListener(listener);
+
+			try {
+				manager.setOntologyLoaderConfiguration(configuration);
+				ontology =
+						manager.loadOntologyFromOntologyDocument(source); //, configuration
+
+			} catch (UnparsableOntologyException e) {
+				System.out.println("Unparsable");
+				e.printStackTrace();
+			} catch (UnloadableImportException importe) {
+				System.out.println("Unloadable Import");
+			} catch (OWLOntologyCreationException e) {
+				System.out.println("Creation failed: " + e.getCause());
+				e.printStackTrace();
+			}
 		}
-		catch(UnparsableOntologyException e){
-			System.out.println("Unparsable");
-			e.printStackTrace();
-		}
-        catch (UnloadableImportException importe){
-            System.out.println("Unloadable Import");
-        }
-		catch (OWLOntologyCreationException e) {
-			System.out.println("Creation failed: " + e.getCause());
+		catch (IOException e) {
+			System.out.print("File " + pathName + " does not exist");
 			e.printStackTrace();
 		}
 		
